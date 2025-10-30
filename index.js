3192d39306ab14af1638c496b5dfbe438c0508a8
zsp367252405 <126630528+zsp367252405@users.noreply.github.com>
2025-10-30 09:07:08 +0800

    Update ARGO_DOMAIN and ARGO_AUTH defaults

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const UUID = process.env.UUID || '28e72fb9-dadb-42b0-9d34-4343fce3658f'; // 使�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'zcq2012.6.8.3.e.5.b.4.a.f.f.0.7.0.0.6.2.ip6.arpa';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNDEwNjZkMzgwMGQ3NzMxMWM2MThlMGIyYmZkMjJlZTkiLCJ0IjoiNjNiNmNmMjgtZTczYy00OGZkLWFhYzItYTFhY2JjOTllMGEyIiwicyI6Ik9UYzNNbVptWXpjdE5UaGlOQzAwTVdZeUxXRTRaR0l0WldRMVpqTTROVGhrTmpZNCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || '';                        // 节点名称
+const NAME = process.env.NAME || 'zcq2012';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
